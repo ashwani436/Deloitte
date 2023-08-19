159dd16fa57e5d937d4886377ca3f9067ac39d9c
ashwani436 <sharmagpl786@gmail.com>
2023-08-19 23:01:42 +0530

    save-changes

---
+++ server-api/jwtConfig.js
@@ -0,0 +1,4 @@
+export default {
+  secretKey: '8d72c8bfec30a961baa1d617cf055c29ce01c13e9dddc5dbd7a4b5c5e6b634f9',
+  expiresIn: '1h', 
+};
---
+++ server-api/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const jwtConfig = require('../../jwtConfig');
+const db = require('../../db.json');
+// const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+// app.use(cors());
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = db.users.find((user) => user.username === username && user.password === password);
+
+  if (user) {
+    const token = jwt.sign({ username: user.username }, jwtConfig.secretKey, {
+      expiresIn: jwtConfig.expiresIn,
+    });
+    res.json({ token });
+  } else {
+    res.status(401).json({ message: 'Invalid username or password' });
+  }
+});
+
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  console.log(username,password);
+  const existingUser = db.users.find((user) => user.username === username);
+  if (existingUser) {
+    return res.status(400).json({ message: 'Username already exists' });
+  }
+
+  db.users.push({ username, password });
+  res.status(201).json({ message: 'Registration successful' });
+});
+
+
+const port = 5000; 
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
